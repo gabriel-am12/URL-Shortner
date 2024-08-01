e1d9ba9ef6341510a5bc805fe329f6035e22c8e5
Gabriel Augusto <58897595+gabriel-am12@users.noreply.github.com>
2024-08-01 12:49:13 +0000

    feat: url.js

---
+++ routes/url.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const { nanoid } = require("nanoid");
+const createDB = require("../config/db");
+const URL = require("../models/urlModel");
+
+const baseUrl =
+  "https://ce0de3a6-7650-4cf9-bfbb-ca3b81d3741a-00-zf2btb93cnoi.spock.replit.dev/urlapi";
+
+createDB.sync().then(() => {
+  console.log("DB is connected");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { longUrl } = req.body;
+    const shortId = nanoid(4);
+    const shortUrl = await URL.create({
+      longUrl,
+      shortUrl: shortId,
+    });
+
+    return res.status(200).json({
+      status: "ok",
+      shortUrl: `${baseUrl}/${shortId}`,
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(e);
+  }
+});
+
+module.exports = router;
